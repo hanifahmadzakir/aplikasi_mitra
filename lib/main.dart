5b73a6490d2c209f8fdab33a62a67dd45f472f7b
Vermaquel <yasafipopo5@gmail.com>
2020-10-16 21:11:40 +0700

    - apply firebase_realtime package - make counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -7,29 +8,15 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
-
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mitra_SIPAS',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Mitra_SIPAS'),
     );
   }
 }
@@ -37,88 +24,192 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  final dbref = FirebaseDatabase.instance.reference();
-  void _incrementCounter() {
-    dbref.child("2").set({
-      'id':'id',
-      'data':'data'
+  // inisiasi seluruh variabel yang akan digunakan: vaiabel kounter, kuota maksimal, database reference
+  int _countermobil;
+  int _countermotor;
+  int _lahanmobil;
+  int _lahanmotor;
+  DatabaseReference _counterRef;
+  DatabaseReference _counterRefMobil;
+  DatabaseReference _counterRefMotor;
+  DatabaseError _error;
+  //DatabaseReference _messagesRef;
+  StreamSubscription<Event> _counterSubscriptionMobil;
+  StreamSubscription<Event> _counterSubscriptionMotor;
+  //StreamSubscription<Event> _messagesSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    // Demonstrates configuring to the database using a file
+    // Menyiapkan referensi untuk pengolahan data dari database
+    final FirebaseDatabase database = FirebaseDatabase();
+    _counterRef = database
+        .reference()
+        .child('test')
+        .child('lahanParkir')
+        .child('namaMitra')
+        .child('kuota');
+    database.reference().child('hey').once().then((DataSnapshot snapshot) {
+      print('Data : ${snapshot.value}');
     });
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    _counterRefMobil = _counterRef.child('kuotaMobil').child('kuotaMobil');
+    _counterRefMotor = _counterRef.child('kuotaMotor').child('kuotaMotor');
+    //_counterRef = FirebaseDatabase.instance.reference().child('hey');
+    // Demonstrates configuring the database directly
+    database.setPersistenceEnabled(true);
+    database.setPersistenceCacheSizeBytes(10000000);
+    _counterRefMobil.keepSynced(true);
+    _counterRefMotor.keepSynced(true);
+
+    // pembacaan data secara realt-time untuk mobil
+    _counterSubscriptionMobil = _counterRefMobil.onValue.listen((Event event) {
+      setState(() {
+        _error = null;
+        _countermobil = event.snapshot.value ?? 0;
+      });
+    }, onError: (Object o) {
+      final DatabaseError error = o;
+      setState(() {
+        _error = error;
+      });
+    });
+
+    // pembacaan data secara realtime motor
+    _counterSubscriptionMotor = _counterRefMotor.onValue.listen((Event event) {
+      setState(() {
+        _error = null;
+        _countermotor = event.snapshot.value ?? 0;
+      });
+    }, onError: (Object o) {
+      final DatabaseError error = o;
+      setState(() {
+        _error = error;
+      });
+    });
+  }
+
+  // untuk menghentikan pembacaan data secara real-time
+  @override
+  void dispose() {
+    super.dispose();
+    _counterSubscriptionMobil.cancel();
+    _counterSubscriptionMotor.cancel();
+  }
+
+  // fungsi tambahan untuk mengambil data batas maksimum kuota lahan
+  Future<void> lahanparkir() async {
+    final FirebaseDatabase database = FirebaseDatabase();
+    //String temp;
+    var eventref = database
+        .reference()
+        .child('test')
+        .child('lahanParkir')
+        .child('namaMitra')
+        .child('kuota');
+    var kmobil = await eventref.child('maksimal').child('mobil').once();
+    _lahanmobil = kmobil.value;
+    var kmotor = await eventref.child('maksimal').child('motor').once();
+    _lahanmotor = kmotor.value;
+  }
+
+  // fungsi untuk manambahkan kuota
+  void _increment(var count) {
+    // penambahan kuota
+    count.runTransaction((MutableData transaction) async {
+      transaction.value = (transaction.value ?? 0) + 1;
+      return transaction;
+    });
+  }
+
+  // fungsi untuk mengurangi kuota
+  void _minus(var count) {
+    // pengurangan kuota
+    count.runTransaction((MutableData transaction) async {
+      transaction.value = (transaction.value ?? 0) - 1;
+      return transaction;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Counter'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: FutureBuilder(
+          future: lahanparkir(),
+          builder: (ctx, snapshot) => snapshot.connectionState ==
+              ConnectionState.waiting
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : ListView(
+            children: <Widget>[
+              Card(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(
+                          Icons.directions_car,
+                          size: 50.0,
+                        ),
+                        title: Text('Kuota Mobil'),
+                        subtitle:
+                        Text('$_countermobil dari $_lahanmobil'),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          FlatButton(
+                              onPressed: () =>
+                                  _increment(_counterRefMobil),
+                              child: Text('Plus')),
+                          FlatButton(
+                              onPressed: () => _minus(_counterRefMobil),
+                              child: Text('Min'))
+                        ],
+                      )
+                    ]),
+              ),
+              Card(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      ListTile(
+                        leading: Icon(
+                          Icons.motorcycle,
+                          size: 50.0,
+                        ),
+                        title: Text('Kuota Motor'),
+                        subtitle:
+                        Text('$_countermotor dari $_lahanmotor'),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          FlatButton(
+                              onPressed: () =>
+                                  _increment(_counterRefMotor),
+                              child: Text('Plus')),
+                          FlatButton(
+                              onPressed: () =>
+                                  _minus(_counterRefMotor),
+                              child: Text('Min'))
+                        ],
+                      )
+                    ]),
+              ),
+            ],
+          )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
